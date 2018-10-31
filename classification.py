566e267cea0397fd9c64b0de3731a434ed6e6764
周天洋 <zhoutianyang@sanshengshi.co>
2018-10-31 22:16:28 +0800

    classification

---
+++ classification.py
@@ -0,0 +1,41 @@
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+
+mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+
+def add_layer(inputs, in_size, out_size, activation_function=None):
+        Weights = tf.Variable(tf.random_normal([in_size, out_size]))
+        biases = tf.Variable(tf.zeros([1, out_size]) + 0.1)
+        Wx_plus_b = tf.matmul(inputs, Weights) + biases
+        return Wx_plus_b if (activation_function == None) else activation_function(Wx_plus_b)
+
+def compute_accuracy(v_xs, v_ys):
+        global prediction
+        y_pre = sess.run(prediction, feed_dict={xs: v_xs})
+        correct_prediction = tf.equal(tf.argmax(y_pre, 1), tf.argmax(v_ys, 1))
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+        return sess.run(accuracy, feed_dict={xs: v_xs, ys: v_ys})
+
+#define placeholder form inputs to network
+xLen = 28 * 28
+yLen = 10
+xs = tf.placeholder(tf.float32, [None, xLen])  # 28*28
+ys = tf.placeholder(tf.float32, [None, yLen])
+
+prediction = add_layer(xs, xLen, yLen, activation_function=tf.nn.softmax)
+
+#the error between prediction and real data
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(ys * tf.log(prediction), reduction_indices=[1]))  # loss
+train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+
+sess = tf.Session()
+sess.run(tf.initialize_all_variables())
+
+for i in range(1000):
+        batch_xs, batch_ys = mnist.train.next_batch(100)
+        sess.run(train_step, feed_dict={xs:batch_xs, ys:batch_ys})
+        if (i % 50 == 0):
+                print(compute_accuracy(
+                        mnist.test.images,
+                        mnist.test.labels
+                ))
